b01e816724995e098c5b69564428e6285e8d8767
alexanderbozhkov <alexander.bozhkov89@gmail.com>
2019-02-03 20:56:13 +0200

    moving the files in the src/

--- 9archive.py
+++
@@ -1,8 +0,0 @@
-# Read the .txt file and extract the meme urls
-
-
-def get_all_memes(meme_file, meme_list: list):
-    with open(meme_file, 'r') as f:
-        for line in f:
-            if line.startswith('http'):
-                meme_list.append(line.strip())
